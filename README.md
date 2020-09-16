03e644daf8b5ee2f2e95ad32b7a516de66abef90
N-911 <nikolayenkoserhiy@gmail.com>
2020-09-16 16:51:22 +0300

    read directory

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CPP-Utag
 ucode c++ project
+
+
+resources : 
+
+https://developer.apple.com/design/human-interface-guidelines/macos/overview/themes/
--- app/main.cpp
+++ app/main.cpp
@@ -1,78 +1,91 @@
 #include <iomanip>
 #include <iostream>
-//#include "tag.h"
-
 //#include "../taglib/tag.h"
 #include "../../taglib/taglib/tag.h"
 #include <../taglib/fileref.h>
 #include <toolkit/tpropertymap.h>
-// #include <tkDecls.h>
-
-//#include <../taglib/tpropertymap.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 using std::cout;
 using std::endl;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    DIR *dp;
+    struct dirent *dirp;
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         return 1;
     }
-    cout << "******************** \"" << argv[1] << "\" ********************" << endl;
 
-    TagLib::FileRef f(argv[1]);
-
-    if (!f.isNull() && f.tag())
-    {
-
-        TagLib::Tag *tag = f.tag();
-
-        cout << "-- TAG (basic) --" << endl;
-        cout << "title   - \"" << tag->title() << "\"" << endl;
-        cout << "artist  - \"" << tag->artist() << "\"" << endl;
-        cout << "album   - \"" << tag->album() << "\"" << endl;
-        cout << "year    - \"" << tag->year() << "\"" << endl;
-        cout << "comment - \"" << tag->comment() << "\"" << endl;
-        cout << "track   - \"" << tag->track() << "\"" << endl;
-        cout << "genre   - \"" << tag->genre() << "\"" << endl;
-
-        TagLib::PropertyMap tags = f.file()->properties();
-
-        unsigned int longest = 0;
-        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
-        {
-            if (i->first.size() > longest)
-            {
-                longest = i->first.size();
-            }
-        }
-
-        cout << "-- TAG (properties) --" << endl;
-        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
-        {
-            for (TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j)
-            {
-                cout << std::left << std::setw(longest) << i->first << " - " << '"' << *j << '"' << endl;
-            }
-        }
+//    struct stat buf;
+    if ((dp = opendir(argv[1])) == NULL) {
+        std::cerr << "error open folder" << endl;
     }
+    while((dirp = readdir(dp)) != NULL) {
 
-    if (!f.isNull() && f.audioProperties())
-    {
+        cout << "******************** \"" << dirp->d_name << "\" ********************" << endl;
 
-        TagLib::AudioProperties *properties = f.audioProperties();
+        TagLib::FileRef f(dirp->d_name);
 
-        int seconds = properties->length() % 60;
-        int minutes = (properties->length() - seconds) / 60;
+        if (!f.isNull() && f.tag()) {
 
-        cout << "-- AUDIO --" << endl;
-        cout << "bitrate     - " << properties->bitrate() << endl;
-        cout << "sample rate - " << properties->sampleRate() << endl;
-        cout << "channels    - " << properties->channels() << endl;
-        cout << "length      - " << minutes << ":" << std::setfill('0') << std::setw(2) << seconds << endl;
+            TagLib::Tag *tag = f.tag();
+
+            cout << "-- TAG (basic) --" << endl;
+
+            cout << "Artist  - \"" << tag->artist() << "\"" << endl;
+            cout << "Title   - \"" << tag->title() << "\"" << endl;
+            cout << "Album   - \"" << tag->album() << "\"" << endl;
+            cout << "Genre   - \"" << tag->genre() << "\"" << endl;
+            cout << "year    - \"" << tag->year() << "\"" << endl;
+            cout << "comment - \"" << tag->comment() << "\"" << endl;
+            cout << "track   - \"" << tag->track() << "\"" << endl;
+
+            TagLib::PropertyMap tags = f.file()->properties();
+
+            unsigned int longest = 0;
+            for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
+                if (i->first.size() > longest) {
+                    longest = i->first.size();
+                }
+            }
+
+            cout << "-- TAG (properties) --" << endl;
+            for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
+                for (TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j) {
+                    cout << std::left << std::setw(longest) << i->first << " - " << '"' << *j << '"' << endl;
+                }
+            }
+        }
+
+        if (!f.isNull() && f.audioProperties()) {
+
+            TagLib::AudioProperties *properties = f.audioProperties();
+
+            int seconds = properties->length() % 60;
+            int minutes = (properties->length() - seconds) / 60;
+
+            cout << "-- AUDIO --" << endl;
+            cout << "bitrate     - " << properties->bitrate() << endl;
+            cout << "sample rate - " << properties->sampleRate() << endl;
+            cout << "channels    - " << properties->channels() << endl;
+            cout << "length      - " << minutes << ":" << std::setfill('0') << std::setw(2) << seconds << endl;
+        }
+        cout << "========================== end ===========================" << endl;
     }
 
     return 0;
 }
+
+/*
+if (lstat(argv[1], &buf) < 0) {
+std::cerr << "ошибка вызова функции lstat" << endl;
+}
+
+if (S_ISDIR(buf.st_mode)) {
+std::cerr << " argv not a folder" << endl;
+return 1;
+}
+
+ */
