2f0d130b5005c6982b5d618350fbbdb4b0a1f0c6
Mykyta Savytskyi <nik965@bigmir.net>
2020-09-16 13:48:03 +0300

    taglib cmake added

--- app/main.cpp
+++ app/main.cpp
@@ -6,56 +6,61 @@
 #include "../../taglib/taglib/tag.h"
 #include <../taglib/fileref.h>
 #include <toolkit/tpropertymap.h>
-#include <tkDecls.h>
+// #include <tkDecls.h>
 
 //#include <../taglib/tpropertymap.h>
-
 
 using std::cout;
 using std::endl;
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
-
-    if (argc != 2) {
+    if (argc != 2)
+    {
         return 1;
     }
     cout << "******************** \"" << argv[1] << "\" ********************" << endl;
 
     TagLib::FileRef f(argv[1]);
 
-    if(!f.isNull() && f.tag()) {
+    if (!f.isNull() && f.tag())
+    {
 
         TagLib::Tag *tag = f.tag();
 
         cout << "-- TAG (basic) --" << endl;
-        cout << "title   - \"" << tag->title()   << "\"" << endl;
-        cout << "artist  - \"" << tag->artist()  << "\"" << endl;
-        cout << "album   - \"" << tag->album()   << "\"" << endl;
-        cout << "year    - \"" << tag->year()    << "\"" << endl;
+        cout << "title   - \"" << tag->title() << "\"" << endl;
+        cout << "artist  - \"" << tag->artist() << "\"" << endl;
+        cout << "album   - \"" << tag->album() << "\"" << endl;
+        cout << "year    - \"" << tag->year() << "\"" << endl;
         cout << "comment - \"" << tag->comment() << "\"" << endl;
-        cout << "track   - \"" << tag->track()   << "\"" << endl;
-        cout << "genre   - \"" << tag->genre()   << "\"" << endl;
+        cout << "track   - \"" << tag->track() << "\"" << endl;
+        cout << "genre   - \"" << tag->genre() << "\"" << endl;
 
         TagLib::PropertyMap tags = f.file()->properties();
 
         unsigned int longest = 0;
-        for(TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
-            if (i->first.size() > longest) {
+        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
+        {
+            if (i->first.size() > longest)
+            {
                 longest = i->first.size();
             }
         }
 
         cout << "-- TAG (properties) --" << endl;
-        for(TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
-            for(TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j) {
+        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
+        {
+            for (TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j)
+            {
                 cout << std::left << std::setw(longest) << i->first << " - " << '"' << *j << '"' << endl;
             }
         }
-
     }
 
-    if(!f.isNull() && f.audioProperties()) {
+    if (!f.isNull() && f.audioProperties())
+    {
 
         TagLib::AudioProperties *properties = f.audioProperties();
 
@@ -71,4 +76,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
