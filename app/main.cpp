c34b0e2c04ea6c6481d67707e9b4d87bf5b03171
N-911 <nikolayenkoserhiy@gmail.com>
2020-09-22 13:47:17 +0300

    connect libtag

--- app/main.cpp
+++ app/main.cpp
@@ -2,17 +2,8 @@
 #include <iostream>
 #include <string>
 
-#include "tag_functions.h"
+#include "src/tag_functions.h"
 #include "src/mainwindow.h"
-
-// #include "../../taglib/taglib/tag.h"
-// #include <../taglib/fileref.h>
-// #include <../taglib/mpeg/id3v2/id3v2tag.h>
-// #include <../taglib/mpeg/id3v2/frames/unsynchronizedlyricsframe.h>
-// #include <../taglib/mpeg/id3v2/id3v2frame.h>
-// #include <../taglib/mpeg/id3v2/frames/attachedpictureframe.h>
-// #include <../taglib/mpeg/mpegfile.h>
-// #include <toolkit/tpropertymap.h>
 
 #include <sys/stat.h>
 #include <dirent.h>
@@ -30,51 +21,25 @@
 using std::cout;
 using std::endl;
 
-#include <iomanip>
-#include <string>
-#include <iostream>
-#include <dirent.h>
 
-#include "src/mainwindow.h"
 
 #include <QApplication>
 int main(int argc, char **argv)
 {
-    //    modify_tag(argv[1]);
-    //    load_lyrics(argv[1]);
-    //    read_tags(argv[1]);
-    //
+//        modify_tag(argv[1]);
+//        load_lyrics(argv[1]);
+//        read_tags(argv[1]);
+
     QApplication app(argc, argv);
     MainWindow window((QString(argv[1])));
     window.show();
 
-    //    QApplication app(argc, argv);
-    //    QWidget *window = new QWidget;
-    //
-    //    QLabel *nameLabel = new QLabel("Open Happiness");
-    //    QLineEdit *firstNameLineEdit= new QLineEdit;
-    //    QLineEdit *lastNameLineEdit= new QLineEdit;
-    //    QSpinBox *ageSpinBox = new QSpinBox;
-    //    ageSpinBox->setRange(1, 100);
-    //    QComboBox *employmentStatusComboBox= new QComboBox;
-    //    QStringList employmentStatus = {"Unemployed", "Employed", "NA"};
-    //    employmentStatusComboBox->addItems(employmentStatus);
-    //    QGridLayout *layout = new QGridLayout;
-    //    layout->addWidget(nameLabel, 0, 0);
-    //    layout->addWidget(firstNameLineEdit, 0, 1);
-    //    layout->addWidget(lastNameLineEdit, 0, 2);
-    //    layout->addWidget(ageSpinBox, 1, 0);
-    //    layout->addWidget(employmentStatusComboBox, 1, 1,1,2);
-    //    window->setLayout(layout);
-    //    window->show();
-    //    DIR *dp;
-    //    struct dirent *dirp;
-    //    if (argc != 2) {
-    //        return 1;
-    //    }
-
     return app.exec();
 }
+
+
+
+
 
 //    if ((dp = opendir(argv[1])) == NULL) {
 //        std::cerr << "error open folder" << endl;
--- app/src/tag_functions.h
+++ app/src/tag_functions.h
@@ -20,94 +20,9 @@
 using std::cout;
 using std::endl;
 
-/* Unsychronised lyrics/text transcription
- * This frame contains the lyrics of the song or a text transcription of other vocal activities.
- * The head includes an encoding descriptor and a content descriptor. The body consists of the actual text.
- * The 'Content descriptor' is a terminated string. If no descriptor is entered,
- * 'Content descriptor' is $00 (00) only. Newline characters are allowed in the text.
- * There may be more than one 'Unsynchronised lyrics/text transcription' frame in each tag,
- * but only one with the same language and content descriptor.
- *
- * <Header for 'Unsynchronised lyrics/text transcription', ID: "USLT">
-    Text encoding       $xx
-    Language            $xx xx xx
-    Content descriptor  <text string according to encoding> $00 (00)
-    Lyrics/text         <full text string according to encoding>
-*/
-
 void read_tags(char *file_name);
-
 void load_lyrics(char *file_name);
-
 void load_cover(char *file_name);
-
-void modify_tag(char *file_name)
-{
-    TagLib::FileRef f(file_name);
-
-    if (!f.isNull() && f.tag())
-    {
-        TagLib::Tag *tag = f.tag();
-        tag->setArtist("MOBY_2");
-        // "Tiesto feat. GALXARA"
-    }
-    f.save();
-}
-
-void read_tags(char *file_name)
-{
-    cout << "============== \"" << file_name << "\" =======================" << endl;
-    TagLib::FileRef f(file_name);
-
-    if (!f.isNull() && f.tag())
-    {
-
-        TagLib::Tag *tag = f.tag();
-
-        cout << "-- TAG (basic) --" << endl;
-        cout << "Artist  - \"" << tag->artist() << "\"" << endl;
-        cout << "Title   - \"" << tag->title() << "\"" << endl;
-        cout << "Album   - \"" << tag->album() << "\"" << endl;
-        cout << "Genre   - \"" << tag->genre() << "\"" << endl;
-        cout << "year    - \"" << tag->year() << "\"" << endl;
-        cout << "comment - \"" << tag->comment() << "\"" << endl;
-        cout << "track   - \"" << tag->track() << "\"" << endl;
-
-        TagLib::PropertyMap tags = f.file()->properties();
-
-        unsigned int longest = 0;
-        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
-        {
-            if (i->first.size() > longest)
-            {
-                longest = i->first.size();
-            }
-        }
-
-        cout << "-- TAG (properties) --" << endl;
-        for (TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i)
-        {
-            for (TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j)
-            {
-                cout << std::left << std::setw(longest) << i->first << " - " << '"' << *j << '"' << endl;
-            }
-        }
-    }
-
-    if (!f.isNull() && f.audioProperties())
-    {
-
-        TagLib::AudioProperties *properties = f.audioProperties();
-
-        int seconds = properties->length() % 60;
-        int minutes = (properties->length() - seconds) / 60;
-
-        cout << "-- AUDIO --" << endl;
-        cout << "bitrate     - " << properties->bitrate() << endl;
-        cout << "sample rate - " << properties->sampleRate() << endl;
-        cout << "channels    - " << properties->channels() << endl;
-        cout << "length      - " << minutes << ":" << std::setfill('0') << std::setw(2) << seconds << endl;
-    }
-}
+void modify_tag(char *file_name);
 
 #endif //UTAG_TAG_FUNCTIONS_H
